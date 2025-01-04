d1388eb1f635f7cc41ca8b31b5d16a56cb40d83a
Rohama Salfi <rohxama@gmail.com>
2025-01-04 22:01:56 +0500

    timer done

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
-var clutter = '';
+function bubbleMaker(){
+    var clutter = '';
 
 for( let i = 1; i <= 119; i++){
     var repeat = Math.floor(Math.random()*10);
@@ -6,3 +7,21 @@ for( let i = 1; i <= 119; i++){
 }
 
 document.querySelector('.bottom-p').innerHTML = clutter;
+}
+
+var timer = 60;
+function startTimer(){
+    setInterval(() => {
+        if(timer > 0){
+            timer--;
+            document.querySelector('#timer').textContent = timer;
+        }      
+        else{
+            clearInterval(timer);
+        }  
+    }, 1000);
+}
+
+
+startTimer();
+bubbleMaker();
